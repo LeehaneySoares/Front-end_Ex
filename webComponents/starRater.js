f5bdd00f67be7af78795474ede66d8aa5d51daf0
Leehaney Soares <leehaney.soares@gmail.com>
2022-04-13 00:58:52 -0300

    feat: criando sistemas de estrelas para praticar com web components

--- webComponents/starRater.js
+++ webComponents/starRater.js
@@ -1,9 +1,9 @@
 class StartRater extends HTMLElement {
   #rater
+  #rating = 1
   #shadow
   #star
   #stars
-  #starRater
   #style
 
   get style () {
@@ -21,6 +21,7 @@ class StartRater extends HTMLElement {
     this.#rater = this.createRater()
     this.#stars = this.createStars() // [span, span, span, span, span]
     this.#stars.forEach(star => this.#rater.appendChild(star)) // Para cada Span estou appendando dentro de rater
+    this.resetRating()
     this.#shadow.appendChild(this.#rater)
     return this.#shadow
   }
@@ -28,6 +29,7 @@ class StartRater extends HTMLElement {
   createRater () {
     this.#rater = document.createElement('div')
     this.#rater.classList.add('star-rater')
+    this.#rater.addEventListener('mouseout', (event) => this.resetRating(event))
     return this.#rater
   }
 
@@ -37,20 +39,47 @@ class StartRater extends HTMLElement {
       this.#star.classList.add('star')
       this.#star.setAttribute('data-value', Number(index) + 1)
       this.#star.innerHTML = '&#9733;'
+
+      this.#star.addEventListener('click', (event) => this.setRating(event))
+      this.#star.addEventListener('mouseover', (event) => this.ratingHover(event))
       return this.#star
     }
     return Array.from({ length: 5 }, createStar) // [span, span, span, span]
   }
 
+  highlightRating () {
+    this.#stars.forEach( star => {
+      star.style.color = this.currentTargetValue >= star.getAttribute('data-value')
+      ? 'yellow'
+      : 'gray'
+    })
+  }
+
+  ratingHover (event) {
+    this.currentTargetValue = event.target.getAttribute('data-value')
+    this.highlightRating()
+  }
+
+  resetRating () {
+    this.currentTargetValue = this.getAttribute('data-rating') || 0
+    this.highlightRating()
+  }
+
   styles () {
     this.#style = document.createElement('style')
     this.#style.textContent = `
-      .star-rater {
-        background-color: red;
+      .star {
+        font-size: 5rem;
+        color: gray;
+        cursor: pointer;
       }
     `
     return this.#style
   }
+
+  setRating (event) {
+    return this.setAttribute('data-rating', event.target.getAttribute('data-value'))
+  }
 }
 
-customElements.define('tag-teste', StartRater)
+customElements.define('star-rater', StartRater)
