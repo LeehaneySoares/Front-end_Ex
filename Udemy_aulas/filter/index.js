06d7ffc7b1cdd8e4ec58a65d0780e828f69e8fec
Leehaney Soares Ferreira Nazaré <leehaney.soares@gmail.com>
2022-04-26 21:47:51 -0300

    Aprendendo a filtrar valores de um array através do método filter()

--- Udemy_aulas/filter/index.js
+++ Udemy_aulas/filter/index.js
@@ -14,4 +14,33 @@ function callbackFilter (valor) {
  */
 const newNumbers = numeros.filter(callbackFilter).sort()
 
-console.log(newNumbers)
+/*
+ * O Filter sempre retorna um array valores menor ou igual ao array original, porém com os valores filtrados
+ */
+
+
+const pessoa = [
+  { nome: 'Leehaney', idade: 24 },
+  { nome: 'Valmira', idade: 22 },
+  { nome: 'Taylor', idade: 10 },
+  { nome: 'Betania', idade: 48 },
+  { nome: 'Jubileu', idade: 60 },
+  { nome: 'Luiz', idade: 62 },
+  { nome: 'maria', idade: 23 },
+  { nome: 'Eduardo', idade: 55 },
+  { nome: 'Letícia', idade: 19 },
+  { nome: 'Rosana', idade: 32 },
+  { nome: 'Wallace', idade: 47 },
+]
+
+const moreFiveLetters = pessoa.filter(person => person.nome.length >= 5 )
+const moreFiftyAge = pessoa.filter(person => person.idade > 50)
+const finishA = pessoa.filter(person => person.nome.endsWith('a'))
+
+/*
+  * A Função endsWith() varre uma string e verifica se existe o que passarmos por parâmetro,
+  * no meu caso precisei verificar a letra 'a', mas podemos passar números, entre outros.
+*/
+
+console.log(moreFiveLetters, moreFiftyAge, finishA)
+
