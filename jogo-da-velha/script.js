8e9e96f1e53287d2b1d13de359d7696fb7c2ad9e
Leehaney Soares Ferreira Nazaré <leehaney.soares@gmail.com>
2022-05-17 23:27:19 -0300

    feat(jogo-da-velha): implementando lógica para o jogo

---
+++ jogo-da-velha/script.js
@@ -0,0 +1,18 @@
+const player = document.querySelector('.player p')
+const inputs = document.querySelectorAll('.bloco')
+
+function changePlayer (index) {
+  if (index % 2 === 0) {
+    return player.textContent = 'X'
+  } else {
+    return player.textContent = 'O'
+  }
+}
+
+inputs.forEach((item, index) => {
+  item.addEventListener('click', function () {
+    item.value = changePlayer(index)
+  })
+})
+
+
