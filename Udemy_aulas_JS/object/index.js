1aa8e13d4582b073616b7c248efd6119d32a0c64
Leehaney Soares Ferreira Nazaré <leehaney.soares@gmail.com>
2022-05-20 21:53:36 -0300

    chore(udemy): revisão sobre objetos

--- Udemy_aulas_JS/object/index.js
+++ Udemy_aulas_JS/object/index.js
@@ -11,17 +11,52 @@
 // console.log(robo.bracos)
 // robo.atirar()
 
-const pirata1 = new Object()
+// const pirata1 = new Object()
 
-pirata1.nome = 'Monkey D. Luffy'
-pirata1.fruta = 'Akuma no mi: Gomu Gomu no mi'
-pirata1.ataque = () => console.log('Gomu gomu no jet pistol!')
+// pirata1.nome = 'Monkey D. Luffy'
+// pirata1.fruta = 'Akuma no mi: Gomu Gomu no mi'
+// pirata1.ataque = () => console.log('Gomu gomu no jet pistol!')
 
-console.log(pirata1.nome)
-console.log(pirata1.fruta)
-pirata1.ataque()
+// console.log(pirata1.nome)
+// console.log(pirata1.fruta)
+// pirata1.ataque()
 
-delete pirata1.fruta
+// delete pirata1.fruta
 
-console.log(pirata1)
+// console.log(pirata1)
 
+/**
+ * factory functions / constructor functions -> São padrões de projeto!!
+ *
+ */
+
+function criaPessoa (nome, sobrenome, idade) {
+  return {
+    nome,
+    sobrenome,
+    idade,
+    get nomeCompleto () {
+      return `${this.nome} ${this.sobrenome}`
+    }
+  }
+}
+
+
+const p1 = criaPessoa('Leehaney', 'Soares', 24)
+
+console.log(p1.nome, p1.sobrenome, p1.idade, p1.nomeCompleto)
+
+function Pessoa (nome, sobrenome, idade) {
+  this.nome = nome
+  this.sobrenome = sobrenome
+  this.idade = idade
+  this.aniversario = () => 2022 - this.idade
+  Object.freeze(this)
+}
+
+const p2 = new Pessoa('Leehaney', 'Soares', 25)
+console.log(p2.nome, p2.idade)
+p2.nome = 'Taylor'
+console.log(p2.nome)
+p2.idade = 50
+console.log(p2.nome, p2.idade)
