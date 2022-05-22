511bddcb81c0fdc387948737b93dc65e8eb017af
Leehaney Soares Ferreira Nazaré <leehaney.soares@gmail.com>
2022-05-22 20:50:01 -0300

    chore(udemy): testando Object.defineProperties com diversas chaves do objeto Robo

--- Udemy_aulas_JS/defineProperty/index.js
+++ Udemy_aulas_JS/defineProperty/index.js
@@ -33,14 +33,52 @@ function Pessoa (nome, sobrenome, idade) {
   })
 }
 
-const p1 = new Pessoa ('Leehaney', 'Soares', 24)
-// console.log(p1.sobrenome)
-// console.log(p1.anoNasc(new Date().getFullYear()))
-// console.log(p1.nomeCompleto())
+// const p1 = new Pessoa ('Leehaney', 'Soares', 24)
+// // console.log(p1.sobrenome)
+// // console.log(p1.anoNasc(new Date().getFullYear()))
+// // console.log(p1.nomeCompleto())
 
-const keys = Object.keys(p1) // Joga as chaves de um objeto dentro de um array, para que possa ser iterável
+// const keys = Object.keys(p1) // Joga as chaves de um objeto dentro de um array, para que possa ser iterável
 
-for (let chave of keys) {
-  console.log(chave)
+// for (let chave of keys) {
+//   console.log(chave)
+// }
+
+/**
+ * DefineProperties
+ */
+
+function Robo (modelo, qtPernas, qtBracos) {
+  this.modelo = modelo
+  this.pernas = qtPernas
+  this.bracos = qtBracos
+  this.atira = () => 'pew pew pew (0011010101)'
+
+  Object.defineProperties(this, {
+    pernas: {
+      enumerable: true,
+      value: qtPernas,
+      writable: false,
+      configurable: true
+    },
+    bracos: {
+      enumerable: true,
+      value: qtBracos,
+      writable: false,
+      configurable: true
+    },
+    atira: {
+      enumerable: false
+    }
+  })
 }
 
+const r1 = new Robo ('Ajax', 2, 4)
+
+
+r1.pernas = 30
+r1.bracos = 20
+r1.bracos = 40
+
+console.log(r1)
+console.log(r1.atira())
