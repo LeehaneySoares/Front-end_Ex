9524ab64619fad88ecad3ba93c5f0561afc13224
Leehaney Soares Ferreira Nazaré <leehaney.soares@gmail.com>
2022-05-22 22:22:31 -0300

    chore(udemy): getters e setters - Utilizando método replace apenas por teste

--- Udemy_aulas_JS/getters-e-setters/index.js
+++ Udemy_aulas_JS/getters-e-setters/index.js
@@ -7,7 +7,7 @@ function VideoGame (marca, modelo, rom, tela, ...jogos) {
   Object.defineProperties(this, {
     marca: {
       get: () => marca,
-      set: (value) => marca = value
+      set: (value) => marca = value.replace('NY', '')
     },
     modelo: {
       get: () => modelo,
