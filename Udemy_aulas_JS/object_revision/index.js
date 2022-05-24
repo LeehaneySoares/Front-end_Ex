3b4638f051fc34db58801208a48b1479e7fdb4e8
Leehaney Soares Ferreira Nazaré <leehaney.soares@gmail.com>
2022-05-24 20:20:14 -0300

    feat(udemy): Revisando alguns métodos importantes em objetos

---
+++ Udemy_aulas_JS/object_revision/index.js
@@ -0,0 +1,9 @@
+const produto = {
+  nome: 'VideoGame',
+  capacidade: '500 GB',
+  jogos: ['Watch Dogs', "Assassin's creed", 'Sonic', 'Huugy wuggy']
+}
+
+const newProduto = produto
+
+console.log(newProduto, produto)
