a4611662e6b1ed7751b95c2eb60564a6d1f6e63d
Leehaney Soares Ferreira Nazaré <leehaney.soares@gmail.com>
2022-05-24 22:22:37 -0300

    feat(udemy): aplicando desconstructing e swap em arrays

--- Udemy_aulas_JS/array/index.js
+++ Udemy_aulas_JS/array/index.js
@@ -1,6 +1,29 @@
-const notas = [10, 6, 8]
+// const notas = [10, 6, 8]
 
-const newNotas = [...notas]
-newNotas.unshift(7)
+// const newNotas = [...notas]
+// newNotas.unshift(7)
 
-console.log(newNotas)
+// console.log(newNotas)
+
+// function sum(u = -1, v = 0, x = 1, y = 2, z = 3) {
+//   return u + v + x + y + z
+// }
+
+// const num = [3, 4, 5]
+// const params = [1, 2, ...num]
+// console.log(sum(...params))
+
+/**
+ * Desconstruction
+ */
+const [x, y] = ['a', 'b']
+
+console.log(x, y)
+
+/**
+ * Swap
+ */
+
+const [a, b] = [y, x]
+
+console.log(a, b)
