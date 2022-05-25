422ba0cb330f54439e2538eee85f5be4c6fccda4
Leehaney Soares Ferreira Nazaré <leehaney.soares@gmail.com>
2022-05-24 22:16:04 -0300

    feat(udemy): aprendendo novos métodos de objeto

--- Udemy_aulas_JS/object_revision/index.js
+++ Udemy_aulas_JS/object_revision/index.js
@@ -1,9 +1,43 @@
-const produto = {
-  nome: 'VideoGame',
-  capacidade: '500 GB',
-  jogos: ['Watch Dogs', "Assassin's creed", 'Sonic', 'Huugy wuggy']
-}
+// const produto = {
+//   nome: 'VideoGame',
+//   capacidade: '500 GB',
+//   jogos: ['Watch Dogs', "Assassin's creed", 'Sonic', 'Huugy wuggy']
+// }
 
-const newProduto = produto
+// const newProduto = { ...produto }
+// newProduto.nome = 'Celular'
 
-console.log(newProduto, produto)
+// // console.log(newProduto)
+// // console.log(produto)
+
+// const newObj = {
+//   nome: 'Leehaney',
+//   sobrenome: 'Soares'
+// }
+
+// const outroObj = {
+//   last: 'Nazaré',
+//   idade: 25
+// }
+
+// const assign = { ...newObj, ...outroObj }
+
+// const produto = { nome: 'caneca', estoque: 5, material: 'Porcelana' }
+// Object.defineProperty(produto, 'estoque', {
+//   enumerable: true,
+//   writable: false,
+//   configurable: false
+// })
+
+// produto.estoque = 50000
+// console.log(Object.getOwnPropertyDescriptor(produto, 'estoque'))
+// console.log(produto)
+
+// const obj = { nome: 'Leehaney', sobrenome: 'Soares', idade: 24 }
+
+// console.log(Object.values(obj))
+// console.log(Object.entries(obj))
+
+// for (let [key, value] of Object.entries(obj)) {
+//   console.log({ [key]: value })
+// }
