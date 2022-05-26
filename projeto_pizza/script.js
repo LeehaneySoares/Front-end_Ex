5a143ff6458a791c10ce15a2dcfc9da8447b9788
Leehaney Soares Ferreira Nazaré <leehaney.soares@gmail.com>
2022-05-26 17:17:03 -0300

    feat(projeto_pizza): renderinzando as imagens e o nome via js

--- projeto_pizza/script.js
+++ projeto_pizza/script.js
@@ -1,3 +1,24 @@
 import pizzaJson from './pizzas.js'
 
+const pizzaArea = document.querySelector('.pizza-area')
+
 console.log(pizzaJson)
+
+function card (image, name) {
+  const img = document.createElement('img')
+  const caption = document.createElement('figcaption')
+  const figure = document.createElement('figure')
+  const a = document.createElement('a')
+  a.textContent = name
+  caption.appendChild(a)
+
+  figure.setAttribute('class', 'pizza-item')
+  img.setAttribute('class', 'pizza-item--img')
+  img.setAttribute('src', `./${image}`)
+  figure.append(img, caption)
+  return figure
+}
+
+pizzaJson.forEach(function (item, index) {
+  pizzaArea.appendChild(card(item.img, item.name))
+})
