ae3112c2c7ae4eb447b182f97aeab95670abe099
Leehaney Soares Ferreira Nazaré <leehaney.soares@gmail.com>
2022-06-02 20:47:51 -0300

    refactor(POO): distribuindo responsabildades para classes especializadas

--- POO-na-pratica/calculadora/script.js
+++ POO-na-pratica/calculadora/script.js
@@ -1,52 +1,31 @@
-import EntradaSaida from "../EntradaSaida.js"
+import EntradaSaida from './EntradaSaida.js'
+import Operacao from './Operacao.js'
+import ExibeMensagem from './ExibeMensagem.js'
 
 function calculadora () {
-  alert('Calculadora sem OO')
+  const message = new ExibeMensagem()
+  const entradaSaida = new EntradaSaida()
 
-  const entradaSaida = EntradaSaida.create() // new EntradaSaida()
+  message.recebeMensagem('Calculadora sem OO')
 
-  let res = entradaSaida.continuar();
+  while (true) {
+    const resposta = entradaSaida.continue()
 
-  console.log(res)
+    if (resposta === 'S' || resposta === 's') break
 
-  // while (true) {
-  //   res = prompt('Digite S ou s para sair: ')
-  //   if (res === 'S' || res === 's') break;
+    /**
+     * Método estático para capiturar valores
+     */
+    entradaSaida.captureValue()
 
-  //   const n1 = parseFloat(prompt('Digite o primeiro número: '))
-  //   const n2 = parseFloat(prompt('Digite o segundo número: '))
-  //   const op = prompt('Digite qual operação deseja realizar -> (+, -, * ou /): ')
+    const op = new Operacao(
+      entradaSaida.n1,
+      entradaSaida.n2,
+      entradaSaida.operation
+    )
 
-  //   let resultado = 0;
-  //   let msg = '';
-  //   switch (op) {
-  //     case '+':
-  //       resultado = n1 + n2
-  //       console.log(`A soma é: ${resultado}`)
-  //     break;
-  //     case '-':
-  //       resultado = n1 - n2;
-  //       console.log(`A subtração é: ${resultado}`)
-  //     break;
-  //     case '*':
-  //       resultado = n1 * n2;
-  //       console.log(`A multiplicação é: ${resultado}`)
-  //     break;
-  //     case '/':
-  //       if (n2 != 0) {
-  //         resultado = n1 / n2
-  //         console.log(`A divisão é: ${resultado}`)
-  //         return msg
-  //       } else {
-  //         resultado = 999999999;
-  //         console.log('Divisão por zero')
-  //         break
-  //       }
-  //     default:
-  //       msg = 'Operação não realizada'
-  //     break
-  //   }
-  // }
+    message.recebeMensagem(op.resultado)
+  }
 }
 
 calculadora()
