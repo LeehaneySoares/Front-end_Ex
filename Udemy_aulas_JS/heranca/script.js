7f4b304ba512ddb1bb6b61926abf4a98f1cf8143
Leehaney Soares Ferreira Nazaré <leehaney.soares@gmail.com>
2022-06-04 14:10:07 -0300

    test(udemy): utilizando Object.defineProperties() com herança

--- Udemy_aulas_JS/heranca/script.js
+++ Udemy_aulas_JS/heranca/script.js
@@ -44,10 +44,49 @@ Carro.prototype.desconto = function (percentual) {
   return this.preco = this.preco - (this.preco * (percentual / 100))
 }
 
-const carro1 = new Carro('HB20', 40000, 2021, 'vermelho')
+// const carro1 = new Carro('HB20', 40000, 2021, 'vermelho')
 
-// carro1.aumento(20)
-carro1.desconto(50)
+// // carro1.aumento(20)
+// carro1.desconto(50)
 
 
-console.log(carro1)
+// console.log(carro1)
+
+function Caneca (nome, preco, material, estoque) {
+  Produto.call(this, nome, preco)
+
+  Object.defineProperties(this, {
+    material: {
+      enumerable: true,
+      get: () => material,
+      set: (value) => typeof value != 'string' ? false : material = value,
+      configurable: false
+    },
+    estoque: {
+      enumerable: true,
+      get: () => estoque,
+      set: (value) => typeof value != 'number' ? false : estoque = value,
+      configurable: false
+    }
+  })
+}
+Caneca.prototype = Object.create(Produto.prototype)
+Caneca.prototype.constructor = Caneca
+
+/**
+ * Vendo se conseguimos atribuir métodos que estão no prototype de outras constructor function
+ * em uma nova constructor function diferente
+ */
+
+Caneca.prototype.aumento = Carro.prototype.aumento
+Caneca.prototype.desconto = Carro.prototype.desconto
+
+const caneca = new Caneca('Caneca', 25, 'porcelana', 5)
+
+// caneca.aumento(20)
+
+//Naõ seta esses valor por conta da verificação dentro do método setter
+caneca.estoque = '100'
+caneca.material = 4234242
+
+console.log(caneca)
