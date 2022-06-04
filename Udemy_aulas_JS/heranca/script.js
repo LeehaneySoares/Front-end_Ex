374baf0cfe21505d0d13f09614f8dd331a81e687
Leehaney Soares Ferreira Nazaré <leehaney.soares@gmail.com>
2022-06-04 12:48:48 -0300

    feat(udemy): aprendendo como javascript faz heranças por debaixo do capô

---
+++ Udemy_aulas_JS/heranca/script.js
@@ -0,0 +1,40 @@
+/**
+ * TODO: Herança (
+  * --> O que todos esses itens abaixo tem em incomum ? São Produtos!
+  * Produto: -> cor, material, ano de fabricação, nome, preço, entre outros.
+  *
+  * --> Camiseta, Caneca, Carro, Computador (Todos São Produtos)
+ * )
+ */
+
+// carro = ano, cor
+// Caneca = material
+
+// Nossa Super Class
+function Produto (nome, preco) {
+  this.nome = nome
+  this.preco = parseFloat(preco)
+}
+Produto.prototype.aumento = function (quantia) {
+  return this.preco += quantia
+}
+Produto.prototype.desconto = function (quantia) {
+  return this.preco -= quantia
+}
+
+function Carro (nome, preco, ano, cor) {
+  Produto.call(this, nome, preco) // é igual a --> super()
+  this.ano = ano
+}
+
+//Funciona também, ele herdou o prototype de produto, mas o contructor não é Carro e sim de Produto
+// Carro.prototype = Produto.prototype
+
+Carro.prototype = Object.create(Produto.prototype)
+Carro.prototype.constructor = Carro
+
+const carro1 = new Carro('HB20', 240, 2021)
+
+// carro1.aumento(200)
+
+console.log(carro1)
