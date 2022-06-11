c210d5c27c09001a4f8134c244e1eef242ac2d30
Leehaney Soares Ferreira Nazaré <leehaney.soares@gmail.com>
2022-06-11 20:28:08 -0300

    refactor(projeto_pizza): removendo pastas não utilizadas

--- projeto_pizza/actions/index.js
+++
@@ -1,8 +0,0 @@
-'use-strict'
-
-export { default as all } from './all.js'
-export { default as $ } from './$.js'
-export { default as changeQt } from './changeQt.js'
-export { default as changeTabs } from './changeTabs.js'
-export { default as setValue } from './setValue.js'
-export { default as toggle } from './toggle.js'
