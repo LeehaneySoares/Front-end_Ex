dde8b3beacf4b68d2601742010d607f8cc3af6ec
Leehaney Soares Ferreira Nazaré <leehaney.soares@gmail.com>
2022-07-01 23:18:28 -0300

    chore(udemy): aprendendo sobre promises

--- Udemy_aulas_JS/promises/index.js
+++ Udemy_aulas_JS/promises/index.js
@@ -11,26 +11,58 @@ const randomTime = (max, min) => (
 
 function esperaAi (msg, tempo) {
   return new Promise((resolve, reject) =>
-    typeof msg !== 'string'
-      ? reject(new Error('A mensagem precisa ser um string'))
-      : setTimeout(() => resolve(msg), tempo)
+    setTimeout(() =>
+      typeof msg !== 'string'
+        ? reject(new Error('A mensagem precisa ser uma string'))
+        : resolve(msg),
+    tempo)
   )
 }
 
-// esperaAi('frase 1', randomTime(1, 3))
-// esperaAi('frase 2', randomTime(1, 3))
-// esperaAi('frase 3', randomTime(1, 3))
-// esperaAi('frase 4', randomTime(1, 3))
+// // esperaAi('frase 1', randomTime(1, 3))
+// // esperaAi('frase 2', randomTime(1, 3))
+// // esperaAi('frase 3', randomTime(1, 3))
+// // esperaAi('frase 4', randomTime(1, 3))
 
-esperaAi('frase 1', randomTime(1, 3))
-  .then((response) => (
-    console.log(response),
-    esperaAi('Frase 2', randomTime(1, 3))
-  ))
-  .then(response => (
-    console.log(response),
-    esperaAi('frase 3', randomTime(1, 4))
-  ))
+// esperaAi('frase 1', randomTime(1, 3))
+//   .then((response) => (
+//     console.log(response),
+//     esperaAi('Frase 2', randomTime(1, 3))
+//   ))
+//   .then(response => (
+//     console.log(response),
+//     esperaAi('frase 3', randomTime(1, 4))
+//   ))
+//   .then(response => console.log(response))
+//   .catch(error => console.log(error))
+
+/**
+ * Métodos uteis para se trabalhar com Promises
+ */
+
+//Promise All
+
+// const teste = [
+//   'Valor qualquer',
+//   esperaAi('Promise 1', randomTime(1, 2)),
+//   esperaAi('Promise 2', randomTime(1, 2)),
+//   esperaAi('Promise 3', randomTime(1, 5)),
+//   'Outro valor qualquer'
+// ]
+
+// Promise.all(teste)
+//   .then(response => console.log(response))
+
+//Promise race
+
+
+const promise = [
+  esperaAi('Eu sou a promise 1', randomTime(1, 5)),
+  esperaAi(1997, randomTime(1, 5)),
+  esperaAi('Eu sou a promise 3', randomTime(1, 2)),
+]
+
+Promise.race(promise)
   .then(response => console.log(response))
   .catch(error => console.log(error))
 
